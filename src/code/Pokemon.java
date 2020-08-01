77dc0afbf7b0887ea6deb8c0754fd827c2a7800c
ryandstermer <ryandstermer@gmail.com>
2020-08-01 10:56:11 -0500

    update to Pokemon.java

--- src/code/Pokemon.java
+++ src/code/Pokemon.java
@@ -3,5 +3,6 @@ package code;
 public class Pokemon {
     public static void main(String[] args){
         System.out.println("Pocket Monsters");
+        System.out.println("Favorite Pokemon by type:");
     }
 }
